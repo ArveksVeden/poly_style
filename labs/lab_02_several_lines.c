faa6ee9aece6a235cdc80123d1a0bdd8ff6df093
Artem Vedeneev <arveksveden@yandex.ru>
2024-09-14 10:52:29 +0300

    решение лабораторной работы 0.2 - вывод нескольких строк - цифровая грамотность

---
+++ labs/lab_02_several_lines.c
@@ -0,0 +1,25 @@
+/*
+лабораторная работа "0.2. Печать нескольких строк"
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("         @\n");
+    printf("        @\n");
+    printf("       @\n");
+    printf("@     @\n");
+    printf(" @   @\n");
+    printf("  @ @\n");
+    printf("   @\n\n");
+    printf("            /\n");
+    printf("           /\n");
+    printf("          /\n");
+    printf("\\        /\n");
+    printf(" \\      /\n");
+    printf("  \\    /\n");
+    printf("   \\  /\n");
+    printf("    \\/\n");
+
+    return 0;
+}
